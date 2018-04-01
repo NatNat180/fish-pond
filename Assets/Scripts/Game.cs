cbdb36eaeae974385a71d73f9335826d8d8075c8
NatNat180 <Nathan.Argall@gmail.com>
2018-04-01 11:16:46 -0500

    logic for hook - needs work

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Game : MonoBehaviour {
 
 	public static bool FishAreCatcheable;
+	public static int Score = 0;
 
 	void Start () {
 		FishAreCatcheable = true;
--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -12,14 +12,30 @@ public class Hook : MonoBehaviour {
 		
 	}
 
+	/*	If hook collides with a fish, begin monitoring of user input.
+
+	 */
 	void OnCollisionEnter(Collision collision) {
 		if (collision.rigidbody.tag == "Fish") {
 			Fish fish = collision.collider.GetComponent<Fish>();
 			KeyCode[] catchCode = fish.CatchReq;
-			Debug.Log("---Catch Requirements---");
-			foreach (KeyCode code in catchCode) {
-				Debug.Log(code);
+			bool isFishCaught = beginCatch(catchCode, fish);
+			if (isFishCaught) {
+				Game.Score += fish.Grade;
+				Destroy(collision.gameObject);
 			}
+			Debug.Log("Current score = " + Game.Score);
 		}
+	}
+
+	bool beginCatch(KeyCode[] catchCode, Fish fish) {
+		int catchProgress = 0;
+		while (catchProgress < catchCode.Length) {
+			if (Input.GetKeyDown(catchCode[catchProgress])) {
+				Debug.Log(catchCode[catchProgress]); 
+				catchProgress++;
+			} else { Debug.Log("try again!"); catchProgress = 0; } 
+		}
+		return true;
 	}
 }
