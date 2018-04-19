21fdb35dfbe1fb1c2a811208d9d22d869ed2fc0e
Argall, Nathan <Nathan.Argall@gmail.com>
2018-04-18 21:50:31 -0500

    move position of fish, take out CoRoutine for while loop in Update function

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -2,57 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hook : MonoBehaviour {
+public class Hook : MonoBehaviour
+{
+    private bool isFishCaught;
+    private static string FISH = "Fish";
+    private bool startTimer;
+    private float timer;
+    private Fish currentCatch;
 
-	private static bool isFishCaught;
-	private static string FISH = "Fish"; 
+    void Start()
+    {
+        isFishCaught = false;
+        startTimer = false;
+		timer = 0;
+        currentCatch = new Fish();
+    }
 
-	void Start () {
-		isFishCaught = false;
-	}
-	
-	void Update () {
-		
-	}
+    void Update()
+    {
+        if (startTimer)
+        {
+            timer -= Time.deltaTime;
+            while (timer > 0)
+            {
+                beginCatch(currentCatch.CatchReq);
+				Debug.Log(timer);
+            }
+        }
+    }
 
-	/*	If hook collides with a fish, freeze fish and begin monitoring 
+    /*	If hook collides with a fish, freeze fish and begin monitoring 
 		of user input -- if catch requirements are met, 
 		reset isFishCaught variable, increment score and 
 		destroy instance of fish -- otherwise, unfreeze fish */
-	void OnTriggerEnter(Collider collider) {
-		if (FISH.Equals(collider.tag)) {
-			Debug.Log("A fish has collided with the hook!");
-			collider.attachedRigidbody.constraints = RigidbodyConstraints.FreezeAll;
-			Fish fish = collider.GetComponent<Fish>();
-			StartCoroutine(BeginCatch(fish.CatchReq, fish.CatchTime));
-			if (isFishCaught) {
-				isFishCaught = false;
-				Game.Score += fish.Grade;
-				Destroy(collider.gameObject);
-			}
-			collider.attachedRigidbody.constraints = RigidbodyConstraints.None;
-		}
-		Debug.Log("Current score = " + Game.Score);
-	}
+    void OnTriggerEnter(Collider collider)
+    {
+        if (FISH.Equals(collider.tag))
+        {
+            Debug.Log("A fish has collided with the hook!");
+            //collider.attachedRigidbody.constraints = RigidbodyConstraints.FreezeAll;
 
-	IEnumerator BeginCatch(KeyCode[] catchCode, int catchTime) { 
-		int countdown = catchTime;
-		int catchProgress = 0;
-		while (countdown > 0) {
-			
-			// start timer
-			yield return new WaitForSeconds(1.0f);
-			countdown--;
-			Debug.Log(countdown);
-			
-			// detect if user is pressing catch code in order - reset progress if wrong key is pressed
-			if (Input.GetKeyDown(catchCode[catchProgress])) {
-				Debug.Log("right key pressed!");
-				catchProgress++;
-			} else { catchProgress = 0; }
-			
-			// if catch progress reaches length of catch code array, fish has been caught
-			if (catchProgress >= catchCode.Length) { isFishCaught = true; }
-		}
-	}
+            Fish fish = collider.GetComponent<Fish>();
+            currentCatch = fish;
+            timer = fish.CatchTime;
+            startTimer = true;
+
+            if (isFishCaught)
+            {
+                isFishCaught = false;
+                Game.Score += fish.Grade;
+                Destroy(collider.gameObject);
+            }
+            //collider.attachedRigidbody.constraints = RigidbodyConstraints.None;
+        }
+        
+		Debug.Log("Current score = " + Game.Score);
+    }
+
+    void beginCatch(KeyCode[] catchCode)
+    {
+        int catchProgress = 0;
+        // detect if user is pressing catch code in order - reset progress if wrong key is pressed
+        if (Input.GetKeyDown(catchCode[catchProgress]))
+        {
+            Debug.Log("right key pressed!");
+            catchProgress++;
+        }
+        else { catchProgress = 0; }
+
+        // if catch progress reaches length of catch code array, fish has been caught
+        if (catchProgress >= catchCode.Length) { isFishCaught = true; }
+    }
+
 }
