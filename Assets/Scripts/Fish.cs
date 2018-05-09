df9e9ac6d5f687364eedb8ce1d71614e168ccefb
NatNat180 <Nathan.Argall@gmail.com>
2018-05-08 23:21:59 -0500

    add NavMeshAgent to BasicFish, give AI movement on mouse click, change fish location on startup

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Fish : MonoBehaviour {
 
@@ -9,9 +10,8 @@ public class Fish : MonoBehaviour {
     private int grade;
     private KeyCode[] catchReq;
     private int catchTime;
-    private Rigidbody fishBody;
-    private bool isAtWayPoint;
-    float time = 3;
+    public NavMeshAgent agent;
+    public Camera cam;
 
 	public string FishName { get {return fishName;} }
 	public int Grade { get {return grade;} }
@@ -19,17 +19,25 @@ public class Fish : MonoBehaviour {
     public int CatchTime { get {return catchTime;} }
 
     void Start() {
-        fishBody = GetComponent<Rigidbody>();
         this.fishName = traits.fishName;
         this.grade = traits.grade;
         this.catchReq = traits.catchReq;
         this.catchTime = traits.catchTime;
         Debug.Log("I am a new fish! My name is " + fishName + ", and my grade is " + grade + "!");
-        isAtWayPoint = false;
     }
 
     void Update() {
-    
+
+        // Testing AI movement with NavMesh 
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                agent.SetDestination(hit.point);
+            }
+        }
+
     }
 
 }
