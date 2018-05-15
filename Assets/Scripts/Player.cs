f7b9d88e2b0646e3d8cb2bf2417b2a01fff531d8
NatNat180 <Nathan.Argall@gmail.com>
2018-05-14 22:27:46 -0500

    add nav mesh point and click AI to Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour {
 
+	public Camera mainCam;
+	private NavMeshAgent playerAgent;
+
 	void Start () {
-		
+		playerAgent = GetComponent<NavMeshAgent>();
 	}
 	
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit)) {
+				playerAgent.SetDestination(hit.point);
+			}
+		}
 	}
 }
