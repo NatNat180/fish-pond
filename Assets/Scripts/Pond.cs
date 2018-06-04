643d3fb9b9062f41f499caef4e89e7b6f943ed57
NatNat180 <Nathan.Argall@gmail.com>
2018-06-03 21:05:24 -0500

    add Pond script, crosshair image to FishingRod folder, formatting changes in scene

---
+++ Assets/Scripts/Pond.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pond : MonoBehaviour {
+
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnMouseOver() {
+		Debug.Log("Mouse has entered!");
+	}
+}
