8c3b71c694c9a0b2a37c506bb7384874c27a2262
NatNat180 <Nathan.Argall@gmail.com>
2018-09-09 22:02:40 -0500

    hook instantiation works with bobber - copies are removed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	public Camera mainCam;
-	private Animator animator;
-	private NavMeshAgent playerAgent;
+    public Camera mainCam;
+    private Animator animator;
+    private NavMeshAgent playerAgent;
     public Rigidbody hook;
     public Transform pond;
 
-	void Start () {
-		playerAgent = GetComponent<NavMeshAgent>();
-		animator = GetComponent<Animator>();
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
 
-	}
+    }
 
     void Update()
     {
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
             if (Physics.Raycast(ray, out hit))
             {
                 playerAgent.SetDestination(hit.point);
@@ -33,10 +34,15 @@ public class Player : MonoBehaviour {
         if (Input.GetButtonDown("Cast"))
         {
             animator.Play("Cast");
-            Vector3 cursorPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            // Make sure y coordinate of hook is consistent with pond
-            cursorPos.y = pond.position.y;
-            Instantiate(hook, cursorPos, Quaternion.identity);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 hitPoint = hit.point;
+                hitPoint.y = pond.position.y;
+                // get rid of any extra instances of hooks
+                Destroy(GameObject.FindGameObjectWithTag("Hook"));
+                Instantiate(hook, hitPoint, Quaternion.identity);
+            }
         }
 
         if (Input.GetButtonDown("CastIdle"))
